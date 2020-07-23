f7b39d601794e338c10e382fce64d4ab07710440
daldoy <pvidalbordoy@gmail.com>
2020-07-23 17:14:30 +0200

    build

--- dist/index.js
+++ dist/index.js
@@ -1272,25 +1272,62 @@ const setFragmentData = (dataTransfer, editor) => {
     // Set a `data-slate-fragment` attribute on a non-empty node, so it shows up
     // in the HTML, and can be used for intra-Slate pasting. If it's a text
     // node, wrap it in a `<span>` so we have something to set an attribute on.
-    if (isDOMText(attach)) {
-        const span = document.createElement('span');
-        // COMPAT: In Chrome and Safari, if we don't add the `white-space` style
-        // then leading and trailing spaces will be ignored. (2017/09/21)
-        span.style.whiteSpace = 'pre';
-        span.appendChild(attach);
-        contents.appendChild(span);
-        attach = span;
-    }
-    const fragment = slate.Node.fragment(editor, selection);
-    const string = JSON.stringify(fragment);
-    const encoded = window.btoa(encodeURIComponent(string));
-    attach.setAttribute('data-slate-fragment', encoded);
-    dataTransfer.setData('application/x-slate-fragment', encoded);
+    // if (isDOMText(attach)) {
+    //   const span = document.createElement('span')
+    //   // COMPAT: In Chrome and Safari, if we don't add the `white-space` style
+    //   // then leading and trailing spaces will be ignored. (2017/09/21)
+    //   span.style.whiteSpace = 'pre'
+    //   span.appendChild(attach)
+    //   contents.appendChild(span)
+    //   attach = span
+    // }
+    // const fragment = Node$1.fragment(editor, selection);
+    // const string = JSON.stringify(fragment);
+    // const encoded = window.btoa(encodeURIComponent(string));
+    // attach.setAttribute('data-slate-fragment', encoded);
+    // dataTransfer.setData('application/x-slate-fragment', encoded);
     // Add the content to a <div> so that we can get its inner HTML.
     const div = document.createElement('div');
     div.appendChild(contents);
-    dataTransfer.setData('text/html', div.innerHTML);
     dataTransfer.setData('text/plain', getPlainText(div));
+    addTimestampAnchor(div);
+    dataTransfer.setData('text/html', div.innerHTML);
+};
+const addTimestampAnchor = (div) => {
+    try {
+        const timestamp = getClosestTimestamp();
+        const a = document.createElement('a');
+        const linkText = document.createTextNode(`[${timestamp}]`);
+        a.appendChild(linkText);
+        a.href = addParamToUrl({
+            urlString: window.location.href,
+            paramKey: 'position',
+            paramValue: timestamp
+        });
+        const space = document.createTextNode(' ');
+        div.insertBefore(space, div.firstChild);
+        div.insertBefore(a, div.firstChild);
+    }
+    catch (e) {
+        console.log('ppp: slate-react/src/components/editable.tsx, error: ', e);
+    }
+};
+const addParamToUrl = ({ urlString, paramKey, paramValue }) => {
+    const url = new URL(urlString);
+    const params = new URLSearchParams(url.search.slice(1));
+    params.set(paramKey, paramValue);
+    const baseUrl = urlString.split("?")[0];
+    return `${baseUrl}?${params.toString()}`;
+};
+const getClosestTimestamp = () => {
+    let node = window.getSelection().anchorNode;
+    for (let i = 0; i < 5; i++) {
+        if (node.getAttribute('data-start')) {
+            return node.getAttribute('data-start');
+        }
+        node = node.parentNode;
+    }
+    throw new Error('ppp: Timestamp not found. slate-react/src/components/editable.tsx');
 };
 /**
  * Get a plaintext representation of the content of a node, accounting for block
